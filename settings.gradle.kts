fccce68fdf42427580ed6a6d4884e3bfef83cc86
Emma-N-Jay <emma.nicole.jeppesen@gmail.com>
2024-01-25 10:30:34 -0800

    Something

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CS371_Lab01"
+include(":app")
+ 
